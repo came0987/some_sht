f23108f65ded694654bd707756e5f2a2cbbbef8e
Gleb <101527763+came0987@users.noreply.github.com>
2026-01-18 22:16:15 +0300

    Update nodes.py

--- nodes.py
+++ nodes.py
@@ -12,3 +12,9 @@ def standard_scaler(data):
 def kmeans(data, n_clusters=3):
     model = KMeans(n_clusters=n_clusters, random_state=42)
     return model.fit_predict(data)
+
+def concat(a, b):
+    return pd.concat([a, b], axis=0)
+
+def weighted_sum(a, b, c, w1=1, w2=1, w3=1): # 3 in
+    return a*w1 + b*w2 + c*w3
